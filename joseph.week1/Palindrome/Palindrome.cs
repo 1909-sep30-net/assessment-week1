9ed35b88739556230813f843e1864ab8d9df1770
Joe Mohrbacher <jhmohrbacher@gmail.com>
2019-10-07 11:14:53 -0500

    set up initial projects and test cases

---
+++ joseph.week1/Palindrome/Palindrome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        public bool IsPalindrom(string s)
+        {
+            return false;
+        }
+    }
+}
---
+++ joseph.week1/PalindromeTest/PalindromeTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xunit;
+using Palindrome;
+
+namespace PalindromeTest
+{
+    [Fact]
+    class PalindromeTest
+    {
+
+    }
+}
