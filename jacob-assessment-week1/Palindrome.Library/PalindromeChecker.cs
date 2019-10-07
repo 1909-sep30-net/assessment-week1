b923230670ee9b788ad9059431e98bb432dd00ec
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-07 11:13:22 -0500

    Implemented additional functionality to handle punctuation and spaces.

--- jacob-assessment-week1/Palindrome.Library/PalindromeChecker.cs
+++ jacob-assessment-week1/Palindrome.Library/PalindromeChecker.cs
@@ -1,14 +1,33 @@
 ﻿using System;
+using System.Text;
 
 namespace Palindrome.Library
 {
     public class PalindromeChecker
     {
+        public static string CleanString(string input)
+        {
+            // Remove all punctuation and spaces.
+            StringBuilder sb = new StringBuilder(); ;
+
+            for (int i = 0; i < input.Length; ++i)
+            {
+                if (!char.IsPunctuation(input[i]) && input[i] != ' ')
+                {
+                    sb.Append(char.ToLower(input[i]));
+                }
+            }
+
+            return sb.ToString();
+        }
+
         public static bool IsPalindrome(string subject)
         {
-            for (int i = 0; i < subject.Length/2; ++i)
+            string clean = CleanString(subject);
+
+            for (int i = 0; i < clean.Length/2; ++i)
             {
-                if (subject[i] != subject[subject.Length - i - 1])
+                if (clean[i] != clean[clean.Length - i - 1])
                     return false;
             }
 
