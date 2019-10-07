ba449eee0d733f8438d421b978eeb81b49c9d83e
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-07 10:59:00 -0500

    Initial commit. Unit tests and palindrome checking functional.

---
+++ jacob-assessment-week1/Palindrome.Library/PalindromeChecker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Palindrome.Library
+{
+    public class PalindromeChecker
+    {
+        public static bool IsPalindrome(string subject)
+        {
+            for (int i = 0; i < subject.Length/2; ++i)
+            {
+                if (subject[i] != subject[subject.Length - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
