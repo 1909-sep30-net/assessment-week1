815a38d24f20aac80ece8bd0ab783dd6781f87be
Mark Moore <mark.c.moore@hotmail.com>
2019-10-07 11:29:14 -0500

    codingQuiz1 init commit

---
+++ palindromeLib/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace palindromQuiz
+{
+    public class Class1
+    {
+
+        public bool Checked(string pal)
+        {
+            string first = pal.Substring(0, pal.Length / 2);
+            char[] arr = pal.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+    }
+}
