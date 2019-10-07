91bae2cf291cd73a82cc98844893f44e7b93bafd
Alvert18 <albertoace_18@outlook.com>
2019-10-07 11:30:30 -0500

    added project files

---
+++ PalindromeLib/PalindromeLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PalindromeLib
+{
+    public class Class1
+    {
+    }
+}
---
+++ PalindromeLib/PalindromeUT/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+
+namespace PalindromeLib.Library
+{
+    public class PalindromeTest
+    {
+        [Fact]
+        public bool 
+        {
+
+
+        }
+    }
+}
