96198f12dd01199b3231acc9f2c8c5d20bc2b30d
Javon Negahban <javon.negahban@cox.net>
2019-10-07 11:24:15 -0500

    Added example strings

--- Palindrome.Test/PalindromeTest.cs
+++ Palindrome.Test/PalindromeTest.cs
@@ -13,6 +13,9 @@ namespace Palindrome.Test
             bool isKeyboardTrue = Palindrome.IsPalindrome("keyboard");
             bool isRacecarWithSpaceTrue = Palindrome.IsPalindrome("race car");
             bool isGumTrue = Palindrome.IsPalindrome("A tin mug for a jar of gum Nita");
+            bool isNursesTrue = Palindrome.IsPalindrome("nurses run");
+            bool isRacecarCapitalizedTrue = Palindrome.IsPalindrome("racecaR");
+            bool isNumbersTrue = Palindrome.IsPalindrome("1221");
 
             Assert.True(isRacecarTrue);
             Assert.False(isIcecreamTrue);
@@ -20,6 +23,9 @@ namespace Palindrome.Test
             Assert.False(isKeyboardTrue);
             Assert.True(isRacecarWithSpaceTrue);
             Assert.True(isGumTrue);
+            Assert.True(isNursesTrue);
+            Assert.True(isRacecarCapitalizedTrue);
+            Assert.True(isNumbersTrue);
         }
     }
 }
