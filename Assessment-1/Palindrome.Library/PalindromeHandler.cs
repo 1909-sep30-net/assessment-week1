0ba38165cf0581925f50df61a0e73ff656a01d2a
Shawn <43576702+saclark12000@users.noreply.github.com>
2019-10-07 11:36:18 -0500

    initial branch upload

---
+++ Assessment-1/Palindrome.Library/PalindromeHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Palindrome.Library
+{
+    public class PalindromeHandler
+    {
+        public Boolean isPalin(string testWord)
+        {
+            string forwardWord = testWord;
+
+            char[] forwardArr;
+            forwardArr = forwardWord.ToCharArray(0, forwardWord.Length);
+
+            
+            int count = 0;
+
+            string backwordWord;
+
+            for (int i = forwardArr.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(forwardArr[i]);
+                //backwordWord =+ forwardArr[i];
+                count++;
+            }
+
+            char[] backwordArr;
+            backwordArr.toCharArray(0, backwordWord.Length);
+
+            int checkCount = 0;
+            for (int i = forwardArr.Length - 1; i >= 0; i--)
+            {
+                if (forwardArr[i] != backwordArr[checkCount])
+                {
+                    return false;
+                }
+                checkCount++;
+            }
+            return false;
+        }
+    }
+}
---
+++ Assessment-1/Palindrome.Test/UnitTest1.cs
@@ -0,0 +1,25 @@
+using System;
+using Xunit;
+
+namespace Palindrome.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //ADD
+
+
+
+            //ACT
+
+
+
+            //ASSERT
+
+
+
+        }
+    }
+}
