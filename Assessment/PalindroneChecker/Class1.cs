1628463798025ad04f7e5639e375f38d84743226
Girard Sanford <girard.sanford@gmail.com>
2019-10-07 11:20:00 -0500

    set up

---
+++ Assessment/PalindroneChecker/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PalindromeChecker.Library
+{
+    public class PalindromeCheck
+    {
+
+        public bool CheckForPalindrome (string s)
+        {
+            string smallS = s.ToLower();
+            //smallS.Replace('.', ' ');
+            //smallS.Replace(',', ' ');
+            char[] arrayS = smallS.ToCharArray();
+            Array.Reverse(arrayS);
+            if(arrayS.ToString() == smallS)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Assessment/PalindroneCheckerTest/UnitTest1.cs
@@ -0,0 +1,21 @@
+using System;
+using Xunit;
+using PalindromeChecker.Library;
+
+namespace PalindromeCheckerTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            string s1 = "nurses  run";
+            string s2 = "racecaR";
+            string s3 = "never odd, or even.";
+            string s4 = "123abccba123";
+            Ch
+            
+
+        }
+    }
+}
