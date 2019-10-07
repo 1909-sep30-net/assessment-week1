3cbb6227295fbfb15b54b17556414baf5619677b
James <oneinfinity@yahoo.com>
2019-10-07 11:30:21 -0500

    commit

---
+++ assessmentWeek1/assessmentWeek1.Library/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace assessmentWeek1.Library
+{
+    class PalindromeTest
+    {
+        public static bool PaliTest(string palindrome)
+        {
+            string firstHalf = palindrome.Substring(0, palindrome.Length / 2);
+            char[] arr = palindrome.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string reverse = new string(arr);
+            string secondHalf = reverse.Substring(0, reverse.Length / 2);
+
+            return firstHalf.Equals(secondHalf);
+        }
+    }
+    }
+}
---
+++ assessmentWeek1/assessmentWeek1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using assessmentWeek1.Library;
+
+//code in branch named according to your first name, lowercase(with last initial as needed).
+
+//two projects, a library project and a unit test project.
+
+//library should have a class that can tell me whether a string is a palindrome or not.
+
+//all the behavior of the class should be unit tested.
+
+//a palindrome reads the same forwards and backwards.
+//for example, these should be considered palindromes:
+//    "nurses run"
+//    "racecaR"
+//    "1221"
+//    "never odd, or even."
+//for example, these should not be considered palindromes:
+//    "one two one"
+//    "123abccba123"
+//follow git and c# best practices.
+
+namespace assessmentWeek1
+{
+    class Program
+    {
+        string testWord;
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter word to be tested: ");
+            testWord = Console.ReadLine():
+
+            Console.WriteLine(testWord + "");
+        }
+    }
+}
