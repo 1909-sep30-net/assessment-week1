790592700a69c106533cfcd817c0b1d6bcb9ca67
yetinight <gang@ucsd.edu>
2019-10-07 11:33:35 -0500

    moved code onto assessment-week1 repo

---
+++ Palindrome/Palindrome.Library/PalindromeChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome.Library
+{
+    public class PalindromeChecker
+    {
+
+        /*
+         * Returns whether or not the given string input is a palindrome
+         * 
+         */
+        public bool isPalindrome(string str)
+        {
+            // check if null
+            if (str == null)
+            {
+                return false;
+            }
+
+            str = removePuncAndSpaces(str);
+
+            // reverse stores str in reverse
+            char[] reverse = str.ToCharArray();
+            Array.Reverse(reverse);
+
+            // if str is a palindrome, 
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] != reverse[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        // Removes punctuation from a string
+        public string removePuncAndSpaces(string str) 
+        {
+            string result = "";
+            for (int i = 0; i < str.Length; i++)
+            {
+                // if current char in str is punctuation or a space, skip inserting current char
+                if (Char.IsPunctuation(str[i]) || str[i] == ' ')
+                {
+                    i += 1;
+                } else
+                {
+                    result += str[i];
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Palindrome/Palindrome.Test/UnitTest1.cs
@@ -0,0 +1,38 @@
+using Palindrome.Library;
+using System;
+using Xunit;
+
+namespace Palindrome.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            PalindromeChecker pal = new PalindromeChecker();
+            string str1 = "abccba";
+            string str2 = "abb";
+
+            //act
+            bool isPal1 = pal.isPalindrome(str1);
+            bool isPal2 = pal.isPalindrome(str2);
+
+            //assert
+            Assert.True(isPal1);
+            Assert.False(isPal2);
+        }
+
+        [Fact]
+        public void Test2()
+        {
+            PalindromeChecker pal = new PalindromeChecker();
+            string str1 = "a,b, s r";
+
+            string str2 = pal.removePuncAndSpaces(str1);
+
+            Assert.Equal("absr", str2);
+
+        }
+    }
+}
