0dd74de26ec49fc8a87bd9e927c8a4f542361443
Javon Negahban <javon.negahban@cox.net>
2019-10-07 10:56:46 -0500

    Added Palindrome library class

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        public static bool IsPalindrome(string str)
+        {
+            string stringWithoutWhiteSpace = str.Replace(" ", "");
+            string reversedString = new string(stringWithoutWhiteSpace.Reverse().ToArray());
+
+            return string.Compare(stringWithoutWhiteSpace, reversedString) == 0;
+        }
+    }
+}
