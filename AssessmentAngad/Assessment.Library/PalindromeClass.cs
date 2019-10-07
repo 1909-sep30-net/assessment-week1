8928c88384aed872c5d141999793b95e32e18019
Angad Singh Minhas <angadsinghminhas@hotmail.com>
2019-10-07 11:14:23 -0500

    Added The Functionality to the Class Library and Tested it with a Test Class

---
+++ AssessmentAngad/Assessment.Library/PalindromeClass.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Assessment.Library
+{
+    public class PalindromClass
+    {
+
+        public string MainString { get; set; }
+
+        public bool CheckPalindrom()
+        {
+            string tempString = MainString;
+            tempString = MainString.Replace(" ", "");
+            tempString = tempString.Replace(",", "");
+            string reversedString = "";
+            int length;
+
+            length = tempString.Length - 1;
+
+            while (length >= 0)
+            {
+                reversedString = reversedString + tempString[length];
+                length--;
+            }
+
+            Console.WriteLine(reversedString);
+
+            if(tempString.ToLower() == reversedString.ToLower())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+    }
+}
--- AssessmentAngad/Assessment.Tests/UnitTest1.cs
+++ AssessmentAngad/Assessment.Tests/UnitTest1.cs
@@ -1,13 +1,37 @@
 using System;
 using Xunit;
+using Assessment.Library;
 
 namespace Assessment.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void IsPalindromeTrueInput()
         {
+            //Arrange
+            PalindromClass testString = new PalindromClass();
+
+
+            //Act
+            testString.MainString = "never odd, or even";
+
+            //Assert
+            Assert.Equal(expected: true, actual: testString.CheckPalindrom()); // This test will pass, since the input string is a Palindrome
+
+        }
+        [Fact]
+        public void IsPalindromeFalseInput()
+        {
+            //Arrange
+            PalindromClass testString = new PalindromClass();
+
+
+            //Act
+            testString.MainString = "ABXYS";
+
+            //Assert
+            Assert.Equal(expected: true, actual: testString.CheckPalindrom()); // This test shall fail, since we expect a Palindrome but the Input is not one
 
         }
     }
