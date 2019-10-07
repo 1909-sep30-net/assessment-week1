cd15da828b44ad289ec439a5b319c83f6a28790a
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-07 11:16:54 -0500

    Added documentation.

--- jacob-assessment-week1/Palindrome.Library/PalindromeChecker.cs
+++ jacob-assessment-week1/Palindrome.Library/PalindromeChecker.cs
@@ -5,9 +5,13 @@ namespace Palindrome.Library
 {
     public class PalindromeChecker
     {
+        /// <summary>
+        /// Removes all punctuation and spaces from a string.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>A 'cleaned' string with no punctuation or spaces.</returns>
         public static string CleanString(string input)
         {
-            // Remove all punctuation and spaces.
             StringBuilder sb = new StringBuilder(); ;
 
             for (int i = 0; i < input.Length; ++i)
@@ -21,6 +25,11 @@ namespace Palindrome.Library
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Checks whether the given string is a palindrome.
+        /// </summary>
+        /// <param name="subject"></param>
+        /// <returns>True if subject is a palindrome, false otherwise.</returns>
         public static bool IsPalindrome(string subject)
         {
             string clean = CleanString(subject);
