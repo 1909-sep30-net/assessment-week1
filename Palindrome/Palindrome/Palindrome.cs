8f026c09d73c723d17c0a68a01084168c0f3d0d4
Victoria de la Guardia <victoriadelaguardia@yahoo.com>
2019-10-07 11:21:00 -0500

    added palindrome class functionality

---
+++ Palindrome/Palindrome/Palindrome.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        static void Main(string[] args)
+        {
+                      
+            string string1, rev;
+            string1 = "racecar";
+            char[] ch = string1.ToCharArray();
+
+            Array.Reverse(ch);
+            rev = new string(ch);
+
+            bool b = string1.Equals(rev, StringComparison.OrdinalIgnoreCase);
+            if (b == true)
+            {
+                Console.WriteLine($"{string1} is a palindrome!");
+            }
+            else
+            {
+                Console.WriteLine($"{string1} is a not palindrome!");
+            }
+
+        }
+}
