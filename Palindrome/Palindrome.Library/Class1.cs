93d35f7b8aab21e1d1c21a81d802ea13115ea259
Alvert18 <albertoace_18@outlook.com>
2019-10-17 09:53:43 -0500

    Adding old uncommitted files with more organized placement

---
+++ Palindrome/Palindrome.Library/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Palindrome.Library
+{
+    public class Pali
+    {
+        public bool IsPalindrome(string value)
+        {
+            int i = 0;
+            int n = value.Length - 1;
+
+            while (i != n)
+            {
+                char x = value[i];
+                char y = value[n];
+                if (Char.ToLower(x) != Char.ToLower(y))
+                {
+                    return false;
+                }
+                i++;
+                n--;
+
+            }
+            return true;
+
+        }
+    }
+}
--- Palindrome/Palindrome.Test/UnitTest1.cs
+++ Palindrome/Palindrome.Test/UnitTest1.cs
@@ -1,13 +1,18 @@
 using System;
 using Xunit;
+using Palindrome.Library;
 
 namespace Palindrome.Test
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("aba")]
+        [InlineData("momma")]
+        public void test1(string value)
         {
+            Pali p = new Pali();
+            Assert.True(p.IsPalindrome(value));
 
         }
     }
